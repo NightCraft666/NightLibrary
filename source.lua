66d97729694d1a452f6c9423435e595572967206
NightCraft <susplacek@gmail.com>
2025-01-24 09:42:59 +0100

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,7 +1,7 @@
 local NightLib = {}
 
 -- Dodaj funkcję sumy
-function NightLib:window()
+function NightLib:window(LoadingText)
     NightLibGui = Instance.new("ScreenGui")
     NightLibMainFrame = Instance.new("Frame")
     NightLibMainFrame.Parent = NightLibGui
@@ -10,6 +10,15 @@ function NightLib:window()
     NightLibMainFrame.Position = UDim2.new(0.5, 0,0.5, 0)
     NightLibMainFrame.Size = UDim2.new(0, 406,0, 257)
     NightLibMainFrame.BackgroundColor3 = Color3.new(255,255,255)
+    NightLibLoadingText = Instance.new("TextLabel")
+    NightLibLoadingText.BackgroundColor3 = Color3.new(255,255,255)
+    NightLibLoadingText.BackgroundTransparency = 1
+    NightLibLoadingText.FontFace.Family = Enum.Font.FredokaOne
+    NightLibLoadingText.Positon = UDim2.new(0.254, 0,0.047, 0)
+    if LoadingText == nil or "" or false then
+        NightLibLoadingText.Text = "NightLibrary"
+    else
+        NightLibLoadingText.Text = tostring(LoadingText)
 end
 
 
